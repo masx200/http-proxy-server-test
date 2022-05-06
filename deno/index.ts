517df9e043c0a68894da4fd95a05238a2cbc1a14
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-06 18:21:52 +0800

    import.meta.main

--- deno/index.ts
+++ deno/index.ts
@@ -1,3 +1,4 @@
 import { listening_port } from "./listening_port.ts";
 import { start } from "./start.ts";
-await start(listening_port);
+//console.log(import.meta)
+if (import.meta.main) await start(listening_port);
--- node/index.ts
+++ node/index.ts
@@ -2,7 +2,7 @@ import process from "process";
 import { listening_port } from "./listening_port.ts";
 
 import { start } from "./start.ts";
-
+//console.log(process)
 process.on("unhandledRejection", (e) => {
     console.error(e);
 });
