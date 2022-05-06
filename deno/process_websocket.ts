71607a5f6bf7adb2adb38b0066f64fd8ed50906b
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-06 14:47:52 +0800

    Update process_websocket.ts

--- deno/process_websocket.ts
+++ deno/process_websocket.ts
@@ -4,6 +4,10 @@ export async function process_websocket(
 ): Promise<Response> {
     const upgrade = req.headers.get("upgrade") || "";
     const connection = req.headers.get("connection") || "";
+    const Sec_WebSocket_Protocol = req.headers
+        .get("Sec-WebSocket-Protocol")
+        ?.replaceAll(" ", "")
+        .split(",");
     if (
         connection.toLowerCase() === "Upgrade".toLowerCase() &&
         upgrade.toLowerCase() === "websocket".toLowerCase()
@@ -19,10 +23,32 @@ export async function process_websocket(
             });
             socket.addEventListener("message", (e) => {
                 console.log(e);
+                client_socket.send(e.data);
             });
             socket.addEventListener("open", (e) => {
                 console.log(e);
             });
+
+            const client_socket = new WebSocket(
+                Object.assign(new URL(req.url), { protocol: "ws:" }).toString(),
+                Sec_WebSocket_Protocol,
+            );
+            console.log(client_socket);
+            client_socket.addEventListener("error", (e) => {
+                console.log(e);
+            });
+            client_socket.addEventListener("close", (e) => {
+                console.log(e);
+                socket.close(e.code, e.reason);
+            });
+            client_socket.addEventListener("message", (e) => {
+                console.log(e);
+                socket.send(e.data);
+            });
+            client_socket.addEventListener("open", (e) => {
+                console.log(e);
+            });
+
             return response;
         } catch (err) {
             console.error(err);
