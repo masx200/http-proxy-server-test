7c1740f69ae00dda36e0d6c65abab33e4a90e198
masx200 <masx200@qq.com>
2022-05-07 13:24:15 +0000

    add deno/defineResponse.ts.

---
+++ deno/defineResponse.ts
@@ -0,0 +1,5 @@
+import{ResponseOptions}from'./ResponseOptions.ts'
+export function defineResponse(options: Partial<ResponseOptions> = {}): ResponseOptions {
+    const { status = 200, statusText, headers = new Headers, body } = options
+    return { status, statusText, headers, body }
+}
