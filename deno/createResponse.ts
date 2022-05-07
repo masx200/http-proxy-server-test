d1e6c63682b69ba07133e847ff22bf02b47613df
masx200 <masx200@qq.com>
2022-05-07 13:25:06 +0000

    add deno/createResponse.ts.

---
+++ deno/createResponse.ts
@@ -0,0 +1,5 @@
+import{ResponseOptions}from'./ResponseOptions.ts'
+export function createResponse(options: ResponseOptions): Response {
+    const { body, ...rest } = options
+    return new Response(body, rest)
+}
