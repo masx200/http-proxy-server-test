3c06febd83df7b967f8d284a8a5770147215a7da
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-08 00:18:34 +0800

    Update Middleware.ts

--- deno/Middleware.ts
+++ deno/Middleware.ts
@@ -1,9 +1,13 @@
-import { ResponseOptions } from "./ResponseOptions.ts";
-
-export type Middleware<Q = Request, S = RetHandler> = (
+import type { ResponseOptions } from "./ResponseOptions.ts";
+import type{ConnInfo}from"https://deno.land/std@0.138.0/http/server.ts";
+export type Middleware<Q = Context, S = RetHandler> = (
     rev: Q,
     next: NextFunction<S>,
 ) => Promise<S> | S;
+export type Context={
 
+connInfo:ConnInfo
+request:Request
+}
 export type NextFunction<S = RetHandler> = () => Promise<S> | S;
 export type RetHandler = Response | ResponseOptions;
