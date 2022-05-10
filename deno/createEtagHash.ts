441f7e9e1b95511c2da1e2b3145eb0878bd73474
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-10 21:14:44 +0800

    Update createEtagHash.ts

--- deno/createEtagHash.ts
+++ deno/createEtagHash.ts
@@ -10,5 +10,6 @@ export async function createEtagHash(
         ? message
         : encoder.encode(message);
     const hashBuffer = await crypto.subtle.digest(algorithm, msgUint8);
-    return msgUint8.length.toString(16)+'-'+decoder.decode(encode(new Uint8Array(hashBuffer)));
+    return msgUint8.length.toString(16) + "-" +
+        decoder.decode(encode(new Uint8Array(hashBuffer)));
 }
