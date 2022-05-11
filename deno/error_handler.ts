b1d8998dd1680e628acb9760e05587ce1e4d3ef1
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-11 12:27:45 +0800

    error_handler

--- deno/error_handler.ts
+++ deno/error_handler.ts
@@ -1,14 +1,12 @@
 import { STATUS_TEXT } from "../deps.ts";
 import { ErrorHandler } from "./ErrorHandler.ts";
-import { Context } from "./Middleware.ts";
 
 // deno-lint-ignore require-await
 export const error_handler: ErrorHandler = async (
     err: unknown,
-    { request }: Context,
 ): Promise<Response> => {
     return new Response(
-        `${STATUS_TEXT.get(500)}\n${request.url}` + "\n" + String(err),
+        `${STATUS_TEXT.get(500)}` + "\n" + String(err),
         {
             status: 500,
         },
--- deno/notfound_handler.ts
+++ deno/notfound_handler.ts
@@ -1,12 +1,12 @@
 import { STATUS_TEXT } from "../deps.ts";
-import { Context } from "./Middleware.ts";
+
 import { NotFoundHandler } from "./NotFoundHandler.ts";
 
 // deno-lint-ignore require-await
-export const notfound_handler: NotFoundHandler = async (
-    { request }: Context,
-): Promise<Response> => {
-    return new Response(`${STATUS_TEXT.get(404)}\n${request.url}`, {
+export const notfound_handler: NotFoundHandler = async (): Promise<
+    Response
+> => {
+    return new Response(`${STATUS_TEXT.get(404)}`, {
         status: 404,
     });
 };
