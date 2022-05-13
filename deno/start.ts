888ed5ae6a5a4a6799e9f8f750bc90cde7d4130e
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-13 14:33:27 +0800

    serve

--- deno/start.ts
+++ deno/start.ts
@@ -1,20 +1,10 @@
-import { serveListener } from "../deps.ts";
+import { serve } from "../deps.ts";
 import { handler } from "./handler.ts";
 
 export async function start(port: number) {
     const servers = [
-        Deno.listen({ port: port, hostname: "0.0.0.0" }),
-        Deno.listen({ port: port, hostname: "::" }),
+        { port: port, hostname: "0.0.0.0" },
+        { port: port, hostname: "::" },
     ];
-    return await Promise.all(servers.map(servestart));
-}
-
-async function servestart(server: Deno.Listener): Promise<void> {
-    try {
-        console.log(`Server listening `, server.addr);
-        await serveListener(server, handler);
-    } catch (error) {
-        console.error(error);
-        return servestart(server);
-    }
+    return await Promise.all(servers.map((server) => serve(handler, server)));
 }
