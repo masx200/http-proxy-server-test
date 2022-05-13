b1db25fe31e4087c5e5f5716dd0dd21e05037b23
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-13 17:31:22 +0800

    Update on_connection.ts

--- deno/on_connection.ts
+++ deno/on_connection.ts
@@ -4,6 +4,9 @@ export async function on_connection(connection: Deno.Conn) {
     const { localAddr, remoteAddr } = connection;
     const connInfo = { localAddr, remoteAddr };
     for await (const requestEvent of Deno.serveHttp(connection)) {
-        await requestEvent.respondWith(handler(requestEvent.request, connInfo));
+        const response = await Promise.resolve(
+            handler(requestEvent.request, connInfo)
+        ).catch(() => new Response(null, { status: 500 }));
+        await requestEvent.respondWith(response);
     }
 }
