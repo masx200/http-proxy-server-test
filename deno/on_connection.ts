0915571483e92c7042630b4cd489da76d5ea29e5
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-13 19:24:21 +0800

    格式化

--- deno/on_connection.ts
+++ deno/on_connection.ts
@@ -5,11 +5,11 @@ export async function on_connection(connection: Deno.Conn) {
     const connInfo = { localAddr, remoteAddr };
     for await (const requestEvent of Deno.serveHttp(connection)) {
         const response = await Promise.resolve(
-            handler(requestEvent.request, connInfo)
-        ).catch((e) =>{
-            console.error(e)
-        return new Response(null, { status: 500 })
-        } );
+            handler(requestEvent.request, connInfo),
+        ).catch((e) => {
+            console.error(e);
+            return new Response(null, { status: 500 });
+        });
         await requestEvent.respondWith(response);
     }
 }
