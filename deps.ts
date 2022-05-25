c79ef4376a3ef8b24d8592d8662b410fb9aa1d89
masx200 <34191203+masx200@users.noreply.github.com>
2022-05-25 09:41:16 +0800

    Update deps.ts

--- deps.ts
+++ deps.ts
@@ -18,19 +18,19 @@ export type {
     Context,
     NextFunction,
     RetHandler,
-} from "https://deno.land/x/masx200_deno_http_middleware@1.1.2/mod.ts";
+} from "https://deno.land/x/masx200_deno_http_middleware@1.1.3/mod.ts";
 export { default as isPlainObject } from "https://cdn.skypack.dev/lodash@4.17.21/isPlainObject?dts";
 export { parse } from "https://deno.land/std@0.140.0/flags/mod.ts";
-export { createHandler } from "https://deno.land/x/masx200_deno_http_middleware@1.1.2/mod.ts";
-export type { Middleware } from "https://deno.land/x/masx200_deno_http_middleware@1.1.2/mod.ts";
+export { createHandler } from "https://deno.land/x/masx200_deno_http_middleware@1.1.3/mod.ts";
+export type { Middleware } from "https://deno.land/x/masx200_deno_http_middleware@1.1.3/mod.ts";
 export {
     conditional_get,
     etag_builder,
     json_builder,
     logger,
     stream_etag,
-} from "https://deno.land/x/masx200_deno_http_middleware@1.1.2/middleware.ts";
+} from "https://deno.land/x/masx200_deno_http_middleware@1.1.3/middleware.ts";
 
-import { error_handler } from "https://deno.land/x/masx200_deno_http_middleware@1.1.2/mod.ts";
+import { error_handler } from "https://deno.land/x/masx200_deno_http_middleware@1.1.3/mod.ts";
 export { error_handler };
-export { notfound_handler } from "https://deno.land/x/masx200_deno_http_middleware@1.1.2/mod.ts";
+export { notfound_handler } from "https://deno.land/x/masx200_deno_http_middleware@1.1.3/mod.ts";
